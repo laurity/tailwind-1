01b50f6553f49acf35b51f397e526882b3a081eb
Laura López <99351746+laurity@users.noreply.github.com>
2024-01-30 11:33:31 +0100

    arreglo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './*.html'
+    './*.html' // add your html file here
   ],
   theme: {
-    extend: {},
+    container: {
+      padding: '1.4rem',
+    },
+    extend: {
+      spacing: {'quarter': '25%',},
+      
+      colors: {
+        'do-blue-dark': '#080c2d',
+        'do-blue-mediumdark': '#1d274c',
+        'do-blue-ligth': 'rgb(0,105,255)',
+        'do-blue-medium': 'rgb(0,125,255)',
+      },
+      fontFamily: {
+        'sans': "'Work Sans', sans-serif",
+        'cascadia': "'Cascadia Code', monospace",
+      },
+    },
   },
   plugins: [],
 }
-
