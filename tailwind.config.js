468fa51b42b3c7500ea32ddd69a90974fe885dba
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-02 12:29:09 +0100

    acabando keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         'do-blue-mediumdark': '#1d274c',
         'do-blue-ligth': 'rgb(0,105,255)',
         'do-blue-medium': 'rgb(0,125,255)',
+        'do-blue-lighter': '#f9fafe',
       },
       boxShadow: {
         'input': '0 5px 1px 0 rgb(0,0,0,0.1)',
@@ -24,6 +25,9 @@ module.exports = {
         'sans': "'Work Sans', sans-serif",
         'cascadia': "'Cascadia Code', monospace",
       },
+      widch: {
+        '88': '22rem',
+      }
     },
   },
   plugins: [],
