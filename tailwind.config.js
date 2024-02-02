326fbccbf0c0ce52781099be4adf3fca45a16dde
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-02 11:36:25 +0100

    acabado

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
         'do-blue-ligth': 'rgb(0,105,255)',
         'do-blue-medium': 'rgb(0,125,255)',
       },
+      boxShadow: {
+        'input': '0 5px 1px 0 rgb(0,0,0,0.1)',
+        'input-hover': '0 2px 1px 0 rgb(0,0,0,0.1)',
+      },
       fontFamily: {
         'sans': "'Work Sans', sans-serif",
         'cascadia': "'Cascadia Code', monospace",
